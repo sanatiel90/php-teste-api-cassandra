fda8dedfe9b511c29f4a2454e5f3abae49018742
sanatiel90 <sanatielbarros@gmail.com>
2017-10-30 21:11:01 -0300

    iniciado app simples para consumir a api

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Página Inicial</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<?php echo "Olá Mundo!"; ?>
+</body>
+</html>
